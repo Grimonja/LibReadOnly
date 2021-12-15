721236f7e2f47d827cb9a74f90f4edaec5f749e6
Grim <bruno.marcetic@gmail.com>
2021-12-15 11:09:57 +0100

    Fix an ooopsie

--- LibReadOnly.lua
+++ LibReadOnly.lua
@@ -6,14 +6,14 @@ if not LibReadOnly then return; end
 
 local makeReadOnly;
 
-local function safePairs(t)
+local function safePairs(lib,t)
 	local lastKey;
 	local function iter()
 		local nextKey, nextValue = next(t,lastKey or nil);
 		if(nextValue)then
 			lastKey = nextKey;
 			if(type(nextValue) == "table")then
-				return nextKey, makeReadOnly(nextValue);
+				return nextKey, makeReadOnly(lib,nextValue);
 			else
 				return nextKey, nextValue;
 			end
@@ -23,14 +23,14 @@ local function safePairs(t)
 	return iter;
 end
 
-local function safeIPairs(t)
+local function safeIPairs(lib,t)
 	local lastIndex = 0;
 	local function iter()
 		lastIndex = lastIndex + 1;
 		local value = t[lastIndex];
 		if(value)then
 			if(type(value) == "table")then
-				return lastIndex, makeReadOnly(value);
+				return lastIndex, makeReadOnly(lib,value);
 			else
 				return lastIndex, value;
 			end
@@ -45,9 +45,9 @@ function makeReadOnly(lib,t,nonRecursive)
 	setmetatable(proxy, {
 		__index = function(self, k)
 			if(k == "pairs")then
-				return function() return safePairs(t); end
+				return function() return safePairs(lib,t); end
 			elseif(k == "ipairs")then
-				return function() return safeIPairs(t); end
+				return function() return safeIPairs(lib,t); end
 			elseif(k == "isReadOnly")then
 				return true;
 			end
